6845f19787207f0edfc56db4da059896de3ae6a2
Jan Killburger <jkillburger@gmail.com>
2023-11-17 17:52:11 +0100

    Added header dark routing logic

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  @Input() darkMode = false;
 
+  ngOnInit(): void {
+    
+  }
 }
