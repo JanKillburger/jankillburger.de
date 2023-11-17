91c9194088fad87b5f250121a11c305542f0731a
Jan Killburger <jkillburger@gmail.com>
2023-11-17 14:46:24 +0100

    Added slideshow component and moved code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['blogger.jpg', 'cyber-glasses.jpg', 'work.jpg'];
-  headlines = [
-    'Bring desperation to the next level',
-    'Bring digitalization to the next level',
-    'Bring programming to the next level'
-  ]
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000)
-  }
+export class AppComponent {
+ 
 }
